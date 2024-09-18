b923f766c3104a5cd38fceeb02c5baacf5f8d0ff
IEVN1001-21000502 <joshua200314@gmail.com>
2024-09-17 22:03:02 -0600

    Angular test

--- baseAngular/src/app/app.module.ts
+++ baseAngular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductoListComponent } from './product/producto-list/producto-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductoListComponent
   ],
   imports: [
     BrowserModule,
