3460f60368474773fbf2b9995f6b069073c51e7b
IEVN1001-21000502 <joshua200314@gmail.com>
2024-09-25 08:55:24 -0600

    distancia

--- baseAngular/src/app/app.module.ts
+++ baseAngular/src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductoListComponent } from './product/producto-list/producto-list.component';
 import { ProductoFilterPipe } from './product/producto-filter.pipe';
+import { EjemploComponent } from './formularios/ejemplo/ejemplo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductoListComponent,
-    ProductoFilterPipe
+    ProductoFilterPipe,
+    EjemploComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
